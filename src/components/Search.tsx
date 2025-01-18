725c3363d861426c70f4e76eb0fc71770c85ddc9
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2025-01-18 20:39:55 -0300

    Adicionando estrutura e fazendo logica de busca

---
+++ src/components/Search.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+type SearchProps = {
+    loadUser: (userName: string) => Promise<void>;
+};
+
+const Search = ({ loadUser }: SearchProps) => {
+
+    const [userName, setUserName] = useState<string>("");
+
+    return (
+        <div>
+            <h2>Busque por usuário:</h2>
+            <p>Conheça seus melhores repositórios</p>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Digite o nome do usuário"
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <button onClick={() => loadUser(userName)}>
+                    <BsSearch />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
